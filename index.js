ed3a8ff8c3002fa805382119f8d989811c944ddd
Matias <matiasezequielsmania@gmail.com>
2025-05-04 21:25:18 -0300

    mas preguntas

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ app.use(cors());
 app.get('/api/questions', (req, res) => {
     fs.readFile('./data/questions.json', 'utf8', (err, data) => {
         if (err) return res.status(500).send('Error al leer las preguntas.');
-        const questions = JSON.parse(data);
-        res.json(questions);
+
+        let questions = JSON.parse(data);
+
+        for (let i = questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [questions[i], questions[j]] = [questions[j], questions[i]];
+        }
+
+        res.json(questions.slice(0, 10));
     });
 });
 
